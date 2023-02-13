ef5fe7bf181e51de7c9c10207b55486948529422
janssenb <bram.janssen@vito.be>
2023-02-13 11:44:46 +0100

    feat: extended script to have more params

--- services/variability_map.py
+++ services/variability_map.py
@@ -17,7 +17,11 @@ def get_script_args():
                         help="Start date of the interval for which to generate the variability maps")
     parser.add_argument("-e", "--end", required=True,
                         help="End date of the interval for which to generate the variability maps")
-    parser.add_argument("-r", "--results", default='../files/variability',
+    parser.add_argument("-b", "--batch", action='store_true',
+                        help="Generate the results in batch mode")
+    parser.add_argument("-r", "--raw", action='store_true',
+                        help="Generate the raw results for the variability map")
+    parser.add_argument("-o", "--output", default='../files/variability',
                         help="File path where to store the results")
     return parser.parse_args()
 
@@ -30,12 +34,16 @@ if __name__ == '__main__':
     args = get_script_args()
 
     features = read_geojson(Path(args.file))
-    dc = setup_openeo().datacube_from_process('variability_map', namespace='vito', polygon=features, date = [args.start, args.end])
+    dc = setup_openeo().datacube_from_process('variability_map', namespace='https://openeo.vito.be/openeo/1.1/processes/u:bramjanssen/variability_map', polygon=features, date = [args.start, args.end], raw=args.raw)
 
-    dc_job = dc.send_job(out_format="GTiff", title=f'Variability Map',
-                                   sample_by_feature=True)
-    dc_job.start_and_wait().get_results().download_files(args.results)
-
+    if args.batch:
+        dc_job = dc.send_job(out_format="GTiff", title=f'Variability Map',
+                                       sample_by_feature=False)
+        dc_job.start_and_wait().get_results().download_files(args.output)
+    else:
+        output = Path(f'{args.output}/result.tiff')
+        output.parent.mkdir(parents=True, exist_ok=True)
+        dc.download(output, format='GTiff')
 
 
 
